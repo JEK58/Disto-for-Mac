014951bb083dc372378fdcd39c738b20ed82e939
Stephan Schöpe <schoepe+xcode@posteo.de>
2019-08-08 11:08:48 +0200

    Initial Commit

---
+++ Disto for Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Disto for Mac
+//
+//  Created by Stephan Schöpe on 08.08.2019.
+//  Copyright © 2019 Stephan Schöpe. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Disto for Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Disto for Mac
+//
+//  Created by Stephan Schöpe on 08.08.2019.
+//  Copyright © 2019 Stephan Schöpe. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
